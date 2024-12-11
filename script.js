1d75711e77a49976291ad49f387fcc5fd263aef2
CodeWithAnniKhan <annipatan@gmail.com>
2024-12-11 18:46:34 +0500

    ready

---
+++ script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let menu = document.querySelector('#menu-bars');
+    let navbar = document.querySelector('.navbar');
+
+    menu.onclick = () => {
+        menu.classList.toggle('fa-times');
+        navbar.classList.toggle('active');
+    };
+
+    window.onscroll = () => {
+        menu.classList.remove('fa-times');
+        navbar.classList.remove('active');
+    };
+
+    document.addEventListener('click', (event) => {
+        // Check if the clicked element is outside the navbar
+        if (!navbar.contains(event.target) && event.target !== menu) {
+            menu.classList.remove('fa-times');
+            navbar.classList.remove('active');
+        }
+    });
+});
+
+
+
+
+
+document.querySelector('#search-icon').onclick = () => {
+    document.querySelector('#search-form').classList.toggle('active') 
+}
+document.querySelector('#close').onclick = () => {
+    document.querySelector('#search-form').classList.remove('active') 
+}
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+
+
+
+
+
+
